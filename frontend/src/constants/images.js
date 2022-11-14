8c9df035d5efd63aee4835dfff3af75fe812d11e
Alessio Lucciola <alessiolucciola98@hotmail.it>
2022-11-14 16:19:13 +0100

    Photos imported

--- frontend/src/constants/images.js
+++ frontend/src/constants/images.js
@@ -1,5 +1,19 @@
 //Import your images here: e.g. 'import logo from 'path_to_img/logo.png'
+import details_emoji from '../../assets/details_emoji.png';
+import face_emoji from '../../assets/face_emoji.png';
+import history_emoji from '../../assets/history_emoji.png';
+import many_faces_emoji from '../../assets/many_faces_emoji.png';
+import pattern_background from '../../assets/pattern_background.png';
+import photo_of_face from '../../assets/photo_of_face.png';
+import selfie_emoji from '../../assets/selfie_emoji.png';
 
 export default {
     //Export your imgs here: e.g. logo
+    details_emoji,
+    face_emoji,
+    history_emoji,
+    many_faces_emoji,
+    pattern_background,
+    photo_of_face,
+    selfie_emoji
 }
