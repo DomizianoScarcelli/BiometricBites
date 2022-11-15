27ae8f5edc3af67ba0306d4c276292104314c6f8
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2022-11-15 16:25:38 +0100

    Refactor in order to have a component

---
+++ frontend/src/components/ProfileIconName/ProfileIconName.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+
+import "./ProfileIconName.scss"
+
+const ProfileIconName = () => {
+	return (
+		<div className="container">
+			<div className="profileIcon">
+				<p>D</p>
+			</div>
+			<p>
+				Good Evening, <br /> Domiziano Scarcelli!
+			</p>
+		</div>
+	)
+}
+
+export default ProfileIconName
--- frontend/src/pages/Homepage/Homepage.tsx
+++ frontend/src/pages/Homepage/Homepage.tsx
@@ -2,11 +2,12 @@ import React from "react"
 
 import "./Homepage.scss"
 import images from "../../constants/images"
+import ProfileIconName from "../../components/ProfileIconName/ProfileIconName"
 
 function Homepage() {
 	return (
 		<div className="background">
-			<NameIcon />
+			<ProfileIconName />
 			<div className="centralContainer">
 				<p>You haven't uploaded any photo yet, upload it in order to start using the recognition system!</p>
 				<button>
@@ -16,19 +17,6 @@ function Homepage() {
 					<img alt="face emoji" src={images.face_emoji}></img>
 				</button>
 			</div>
-		</div>
-	)
-}
-
-const NameIcon = () => {
-	return (
-		<div className="container">
-			<div className="profileIcon">
-				<p>D</p>
-			</div>
-			<p>
-				Good Evening, <br /> Domiziano Scarcelli!
-			</p>
 		</div>
 	)
 }
