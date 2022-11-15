e963b5e04537f04cbd377f40925ebcccf3d83d91
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2022-11-15 16:21:01 +0100

    Implemented Home - No photos and modified some errors in images.js

--- frontend/src/pages/Homepage/Homepage.tsx
+++ frontend/src/pages/Homepage/Homepage.tsx
@@ -1,13 +1,36 @@
-import React from 'react';
+import React from "react"
 
-import './Homepage.scss';
+import "./Homepage.scss"
+import images from "../../constants/images"
 
 function Homepage() {
-  return (
-    <>
-        <p>Homepage text</p>
-    </>
-  )
+	return (
+		<div className="background">
+			<NameIcon />
+			<div className="centralContainer">
+				<p>You haven't uploaded any photo yet, upload it in order to start using the recognition system!</p>
+				<button>
+					<p>
+						Upload a photo of <br /> your face!
+					</p>
+					<img alt="face emoji" src={images.face_emoji}></img>
+				</button>
+			</div>
+		</div>
+	)
+}
+
+const NameIcon = () => {
+	return (
+		<div className="container">
+			<div className="profileIcon">
+				<p>D</p>
+			</div>
+			<p>
+				Good Evening, <br /> Domiziano Scarcelli!
+			</p>
+		</div>
+	)
 }
 
 export default Homepage
