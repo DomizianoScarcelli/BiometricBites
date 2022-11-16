5e1c6b0e2ac331d4d6a1a85ac8f7a31aafde2dd8
Alessio Lucciola <alessiolucciola98@hotmail.it>
2022-11-16 21:56:49 +0100

    Login API added

--- backend/api/utils/dbconnector/dbconnector.py
+++ backend/api/utils/dbconnector/dbconnector.py
@@ -4,3 +4,4 @@ from bsproject import settings
 def dbconnector():
     DBconf = settings.DATABASES.get("mysql")
     return sql.connect(host=DBconf.get("DB_HOST"), username=DBconf.get("DB_USERNAME"), passwd=DBconf.get("DB_PASSWORD"), database=DBconf.get("DB_NAME"))
+
