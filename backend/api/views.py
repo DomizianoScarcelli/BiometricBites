a0284f0b5ffbffbe40497bbf34954c59fdb4ce0b
Anomalyzed <corsi.1742375@studenti.uniroma1.it>
2022-12-01 17:58:01 +0100

    Trying frontend

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,6 +1,5 @@
 #Define all the API here by creating a new function with the name of the API
 from django.http import JsonResponse
-from .utils.recognition.faces import *
 
 def api(request, *args, **kwargs):
     return JsonResponse({'message': 'Test Api'})
