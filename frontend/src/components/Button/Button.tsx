3e71ff467d0372abec969234091d26db7fa252e2
Alessio Lucciola <alessiolucciola98@hotmail.it>
2022-12-08 21:52:57 +0100

    Solved a small bug in the Button component

--- frontend/src/components/Button/Button.tsx
+++ frontend/src/components/Button/Button.tsx
@@ -15,8 +15,8 @@ const Button = ({ text, img, shadow, onClick }: ButtonProps) => {
 			<button style={shadow ? { boxShadow: "4px 5px 19px rgba(var(--shadow-color-rgb), 0.5)" } : {}} onClick={onClick}>
 				<p>
 					{/* Strange trick to use \n in the text props */}
-					{text.split("\n").map((str) => (
-						<p>{str}</p>
+					{text.split("\n").map((str, index) => (
+						<p key={"row"+index}>{str}</p>
 					))}
 				</p>
 				{img ? <img alt={`${img}`} src={img}></img> : <></>}
