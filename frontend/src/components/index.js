38af146f0888b906a54a258a33dcf4c55dfe8d5c
AlessioLucciola <alessiolucciola98@hotmail.it>
2022-12-09 08:44:12 +0100

    Navbar component removed

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -1,4 +1,3 @@
-export { default as Navbar } from './Navbar/Navbar';
 export { default as Popup } from './Popup/Popup';
 export { default as BackButton } from './BackButton/BackButton';
 export { default as LogoutButton } from './LogoutButton/LogoutButton';
