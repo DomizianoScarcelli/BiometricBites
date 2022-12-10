8709a3688254e31bd9ab3816c0c32337b68b1970
Alessio Lucciola <alessiolucciola98@hotmail.it>
2022-12-10 16:02:27 +0100

    Added a function to retrieve user info

---
+++ backend/api/utils/user_info/user_info.py
@@ -0,0 +1,32 @@
+from ..isee_to_cost_calculator import cost_calculator
+from ..dbconnector import dbconnector as db
+
+def get_user_info(user_id):
+    output_data = {
+        "ID": "",
+        "NAME": "",
+        "SURNAME": "",
+        "CF": "",
+        "COST": ""
+    }
+
+    if user_id is None:
+        return ValueError("You must specify the ID of the user.")
+    conn = db.dbconnector()
+    cursor = conn.cursor()
+    query = "SELECT id, name, surname, cf, isee FROM users_info WHERE id='{}'".format(user_id)
+    cursor.execute(query)
+    ret = tuple(cursor.fetchall())
+    if not ret:
+        return ValueError("User not found.")
+    else:
+        ret = ret[0]
+        output_data["ID"] = ret[0]
+        output_data["NAME"] = ret[1]
+        output_data["SURNAME"] = ret[2]
+        output_data["CF"] = ret[3]
+        output_data["COST"] = cost_calculator.cost_calculator(ret[4])
+        print(output_data)
+        return output_data
+
+get_user_info(11)
