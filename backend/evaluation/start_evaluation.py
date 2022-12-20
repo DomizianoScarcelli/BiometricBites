8d2ef44d201c6f4d81032992d9ed6dd4caf9dbb9
Alessio Lucciola <alessiolucciola98@hotmail.it>
2022-12-20 19:34:44 +0100

    Added ROC curve algorithm

---
+++ backend/evaluation/start_evaluation.py
@@ -0,0 +1,33 @@
+from evaluation import open_set_identification_eval, verification_eval
+from matplotlib import pyplot as plt
+
+template_list = [] #TO DEFINE!!
+
+def roc_auc_score(thresholds, eval_type, alg_name):
+    '''
+        Input:
+        thresholds: the list of thresholds to test (e.g. [0.05, 0.10, 0.15, ..., 0.90, 0.95])
+        eval_type: openset, verification
+        alg_name: e.g. Keras
+    '''
+    FAR_list = []
+    FRR_list = []
+    if eval_type == "openset":
+        for threshold in thresholds:
+            metrics = open_set_identification_eval(template_list, threshold)
+            FAR_list.append(metrics["FAR"])
+            FRR_list.append(metrics["FRR"])
+    elif eval_type == "verification":
+        for threshold in thresholds:
+            metrics = verification_eval(template_list, threshold)
+            FAR_list.append(metrics["FAR"])
+            FRR_list.append(metrics["FRR"])
+    else:
+        TypeError("Please, specify a correct evaluation type (either openset or verification)")
+
+
+    plt.plot(FAR_list, FRR_list, marker=".")
+    plt.xlabel('False Acceptance Rate')
+    plt.ylabel('True Rejection Rate')
+    plt.title('ROC curve for ' + alg_name)
+    plt.show()
