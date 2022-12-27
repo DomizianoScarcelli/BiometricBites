acf255a6774550d02beb7c2e073e442d92cd20d4
Alessio Lucciola <alessiolucciola98@hotmail.it>
2022-12-27 21:55:02 +0100

    Some changes

--- backend/evaluation/evaluation.py
+++ backend/evaluation/evaluation.py
@@ -1,15 +1,4 @@
-from sklearn.model_selection import train_test_split, GridSearchCV
-from scipy.spatial import distance
-from evaluation import Gallery
-
-#THE WHOLE EVALUATION STRUCTURE WILL BE CHANGED DEPENDING ON THE RECOGNITION ALGORITHM WE CHOOSE
-#gallery_set = Gallery() #To implement
-#probe_set = Gallery()
-gallery_set = []
-#probe_set = []
-
-#X_train, X_test, y_train, y_test = train_test_split(gallery.get_images(), labels)
-
+#Used to compute the similarieties among all combinations probe/templates
 def compute_similarities(template_list):
     all_similarities = []
     genuine_claims = 0
@@ -20,11 +9,11 @@ def compute_similarities(template_list):
         for template_j, j in enumerate(0, len(template_list)): #for every column (template)
             cur_template = template_j
             if i != j: #Do not consider main diagonal elements
-                if cur_probe == cur_template: #We have to compare the identities to template_list must be some labels..
+                if cur_probe == cur_template: #We have to compare the identities so they must be some labels..
                     genuine_claims += 1
                 else:
                     impostor_claims += 1
-            row_similarities.append(0) #Must substitute 0 with the similarity algorithm
+            row_similarities.append(0) #Must substitute 0 with the similarity algorithm (e.g. the recognition output)
         all_similarities.append(row_similarities)
     return genuine_claims, impostor_claims, all_similarities
 
@@ -95,4 +84,4 @@ def verification_eval(template_list, threshold):
     GRR = GR / impostor_claims
     FAR = FA / impostor_claims
     FRR = FR / genuine_claims
-    return GA, GR, FA, FR
+    return GAR, GRR, FAR, FRR
