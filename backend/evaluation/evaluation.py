c1c743c7044015be5f041120a52e87d55295fb71
Alessio Lucciola <alessiolucciola98@hotmail.it>
2023-01-05 19:33:27 +0100

    Few changes

--- backend/evaluation/evaluation.py
+++ backend/evaluation/evaluation.py
@@ -1,34 +1,37 @@
-#Used to compute the similarieties among all combinations probe/templates
-def compute_similarities(template_list):
+from tqdm import tqdm
+import numpy as np
+
+def compute_similarities(template_list, similarity_function: callable):
     all_similarities = []
     genuine_claims = 0
     impostor_claims = 0
-    for template_i, i in enumerate(0, len(template_list)): #for every row (probe)
+    for i, element_i in enumerate(tqdm(template_list)): #for every row (probe)
+        label_i, template_i = element_i
         row_similarities = []
-        cur_probe = template_i
-        for template_j, j in enumerate(0, len(template_list)): #for every column (template)
-            cur_template = template_j
+        for j, element_j in enumerate(template_list): #for every column (template)
+            label_j, template_j = element_j
             if i != j: #Do not consider main diagonal elements
-                if cur_probe == cur_template: #We have to compare the identities so they must be some labels..
+                if label_i == label_j: #We have to compare the identities to template_list must be some labels..
                     genuine_claims += 1
                 else:
                     impostor_claims += 1
-            row_similarities.append(0) #Must substitute 0 with the similarity algorithm (e.g. the recognition output)
+            similarity = similarity_function(template_i, template_j)
+            row_similarities.append(similarity) #Must substitute 0 with the similarity algorithm
         all_similarities.append(row_similarities)
     return genuine_claims, impostor_claims, all_similarities
 
 #OpenSet Identification Multiple Template
-def open_set_identification_eval(template_list, threshold):
+def open_set_identification_eval(template_list, threshold): 
     genuine_claims, impostor_claims, all_similarities = compute_similarities(template_list)
     DI = [0 for _ in range(template_list)] #Detection and Identification
     GR = FA = 0
-    for template_i, i in enumerate(0, len(template_list)): #for every row (probe)
+    for i, template_i in enumerate(template_list): #for every row (probe)
         cur_probe = template_i
         first_similarity = all_similarities[i][0] #L_i,1, the most similar template
         if first_similarity >= threshold:
             if cur_probe == cur_template: #the identity!! (to change)
                 DI[0] += 1
-                for template_j, j in enumerate(1, len(template_list)): #Parallel impostor case: jump the templates belonging to label(i) since i not in G
+                for j, template_j in enumerate(template_list): #Parallel impostor case: jump the templates belonging to label(i) since i not in G
                     cur_template = template_j
                     k = None
                     if i != j: #Do not consider main diagonal elements so the case in which the template is compared to itself
--- backend/evaluation/start_evaluation.py
+++ backend/evaluation/start_evaluation.py
@@ -1,13 +1,50 @@
-from .evaluation import open_set_identification_eval, verification_eval
-from .plots import roc_auc_score
+from .evaluation import open_set_identification_eval, verification_eval, compute_similarities
+from .plots import roc_curve, far_frr_curve
+from sklearn.datasets import fetch_lfw_people
+from deepface import DeepFace
+from tqdm import tqdm
 import numpy as np
 import pandas as pd
 
-template_list = [] #TO DEFINE!!
+"""
+#---------Update Dataset-----------
+lfw_people = fetch_lfw_people(color=True, min_faces_per_person=10)
+
+# Introspect the images arrays to find the shapes (for plotting)
+n_samples, h, w, c = lfw_people.images.shape
+
+X = lfw_people.images
+n_features = X.shape[1]
+
+# The label to predict is the id of the person
+y = lfw_people.target
+target_names = lfw_people.target_names
+n_classes = target_names.shape[0]
+
+print(X.shape)
+print(y.shape)
+
+data = np.array([(y[i], X[i]) for i in range(len(lfw_people.data))])
+
+def get_similarity_between_two(img1, img2):
+    try:
+        return 1 - DeepFace.verify(img1, img2)["distance"]
+    except:
+        return None
+
+genuine_claims, impostor_claims, all_similarities = compute_similarities(data, get_similarity_between_two)
+
+np.save('similarities', np.array(all_similarities)) #Save the array in order to compute it only once in a lifetime lol
+print("Genuine claims", genuine_claims)
+print("Impostor claims", impostor_claims)
+
+# sim = np.load("./similarities.npy", allow_pickle=True)
+"""
+template_list = {} #To change
 
 open_set_identification_metrics_by_thresholds = {}
 verification_metrics_by_thresholds = {}
-thresholds = np.arange(0.05, 1, 0.05)
+thresholds = np.arange(0.05, 1, 0.05) #To increase (Maybe take a step of just 0.01 to make the plots denser - only if it doesn't take too much time!)
 
 for threshold in thresholds:
     DIR, FRR, FAR, GRR = open_set_identification_eval(template_list, threshold)
@@ -19,9 +56,13 @@ for threshold in thresholds:
 open_set_metrics = pd.DataFrame(open_set_identification_metrics_by_thresholds)
 open_set_FAR_FRR = {"FAR": open_set_metrics.iloc[2], "FRR": open_set_metrics.iloc[1]}
 print("ROC curve for Open Set Identification:")
-roc_auc_score(thresholds, "openset", "VggFace", open_set_FAR_FRR)
+roc_curve("openset", "VggFace", open_set_FAR_FRR)
+print("FAR vs FRR curve for Open Set Identification:")
+far_frr_curve("openset", "VggFace", open_set_FAR_FRR, thresholds)
 
 verification_metrics = pd.DataFrame(open_set_identification_metrics_by_thresholds)
 verification_FAR_FRR = {"FAR": verification_metrics.iloc[2], "FRR": verification_metrics.iloc[1]}
 print("ROC curve for Verification:")
-roc_auc_score(thresholds, "verification", "VggFace", verification_FAR_FRR)
+roc_curve("verification", "VggFace", verification_FAR_FRR)
+print("FAR vs FRR curve for Verification:")
+far_frr_curve("openset", "VggFace", open_set_FAR_FRR, thresholds)
