faf2e62c073ca9a8a05735c016700850ae5569a5
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2023-01-05 12:47:16 +0100

    First steps to try performance evaluation with DeepFace and lfw dataset

--- backend/evaluation/main.py
+++ backend/evaluation/main.py
@@ -2,14 +2,17 @@ from deepface import DeepFace
 from sklearn.datasets import fetch_lfw_people
 import numpy as np
 import matplotlib.pyplot as plt
-lfw_people = fetch_lfw_people()
+from .evaluation import compute_similarities
+from tqdm import tqdm
+
+#TODO: I reduced the dataset to only people with at least 10 photos, and it takes around 15 minutes execute compute_similarities
+# this may be reduced using some numpy vectorization but I don't know how to do it lol. 
+lfw_people = fetch_lfw_people(color=True, min_faces_per_person=10)
 
 # introspect the images arrays to find the shapes (for plotting)
-n_samples, h, w = lfw_people.images.shape
+n_samples, h, w, c = lfw_people.images.shape
 
-# for machine learning we use the 2 data directly (as relative pixel
-# positions info is ignored by this model)
-X = lfw_people.data
+X = lfw_people.images
 n_features = X.shape[1]
 
 # the label to predict is the id of the person
@@ -17,14 +20,24 @@ y = lfw_people.target
 target_names = lfw_people.target_names
 n_classes = target_names.shape[0]
 
-# for index, face_1 in enumerate(X):
-#     for index_2, face_2 in enumerate(X):
-#         print(f"Indexes", index, index_2)
-#         if index != index_2:
-#             face_1 = face_1.reshape((h,w))
-#             face_2 = face_2.reshape((h,w))
-#             print(DeepFace.verify(face_1.reshape((h,w)), face_2.reshape((h,w))))
+print(X.shape)
+print(y.shape)
 
-plt.imshow(X[0].reshape((h, w)))
-plt.show()
 
+# Each element is of type (label, img_array)
+data = np.array([(y[i], X[i]) for i in range(len(lfw_people.data))])
+
+
+def get_similarity_between_two(img1, img2):
+    try:
+        return 1 - DeepFace.verify(img1, img2)["distance"]
+    except:
+        return None
+
+genuine_claims, impostor_claims, all_similarities = compute_similarities(data, get_similarity_between_two)
+
+np.save('similarities', np.array(all_similarities)) #Save the array in order to compute it only once in a lifetime lol
+print("Genuine claims", genuine_claims)
+print("Impostor claims", impostor_claims)
+
+# sim = np.load("./similarities.npy", allow_pickle=True)
