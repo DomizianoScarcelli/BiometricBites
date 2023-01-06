874793f8ba6f505db495b23dcbbf2bd6e4c9a102
Domiziano Scarcelli <scarcelli.domiziano@gmail.com>
2023-01-06 10:45:04 +0100

    Now deepface works with array images

--- backend/evaluation/main.py
+++ backend/evaluation/main.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from .evaluation import compute_similarities
 from tqdm import tqdm
+import cv2
 
 #TODO: I reduced the dataset to only people with at least 10 photos, and it takes around 15 minutes execute compute_similarities
 # this may be reduced using some numpy vectorization but I don't know how to do it lol. 
@@ -23,15 +24,18 @@ n_classes = target_names.shape[0]
 print(X.shape)
 print(y.shape)
 
+X *= 255
+X = np.array(X, dtype='uint8')
 
 # Each element is of type (label, img_array)
 data = np.array([(y[i], X[i]) for i in range(len(lfw_people.data))])
 
-
+model = DeepFace.build_model('VGG-Face')
 def get_similarity_between_two(img1, img2):
     try:
-        return 1 - DeepFace.verify(img1, img2)["distance"]
+        return 1 - DeepFace.verify(img1, img2, model=model, prog_bar=True, detector_backend="dlib")["distance"]
     except:
+        print("Not found")
         return None
 
 genuine_claims, impostor_claims, all_similarities = compute_similarities(data, get_similarity_between_two)
@@ -41,3 +45,15 @@ print("Genuine claims", genuine_claims)
 print("Impostor claims", impostor_claims)
 
 # sim = np.load("./similarities.npy", allow_pickle=True)
+# print(np.count_nonzero(sim != None))
+
+# Change each image in X in order to be compatible with DeepFace
+
+
+# img = X[10]
+# img_load = cv2.imread("./img1.png")
+
+# res1 = DeepFace.analyze(img, detector_backend="dlib")
+
+# res = DeepFace.verify(img1, img2)
+# print(res1)
