b74b26012575dfe05593324e1ba6665a80772b68
Anomalyzed <corsi.1742375@studenti.uniroma1.it>
2023-01-12 17:29:42 +0100

    Applied concatenation on face+eyes+nose+mouth

--- backend/evaluation/face_eyes_nose_mouth_test.py
+++ backend/evaluation/face_eyes_nose_mouth_test.py
@@ -15,48 +15,50 @@ from scipy.stats import pearsonr
 eyes_detector = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_eye.xml")
 nose_detector = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_mcs_nose.xml")
 mouth_detector = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_mcs_mouth.xml")
-print(cv2.data.haarcascades)
 
 # get the faces detected in the image
 def capture_face_features(img):
-    face = img.copy()
-
+    face_hist = extract_histogram(img)
     eyes_hist = []
     nose_hist = []
     mouth_hist = []
-    f = plt.figure()
 
     # eyes
     eyes = eyes_detector.detectMultiScale(img, scaleFactor=1.1, minNeighbors=3)
-    for (x,y,w,h) in eyes:
-        plt.imshow(img[y:y+h, x:x+w])
+    if len(eyes) >= 2:
+        x, y, w, h = eyes[0]
+        eye_1 = extract_histogram(img[y:y+h, x:x+w])
+        x, y, w, h = eyes[1]
+        eye_2 = extract_histogram(img[y:y+h, x:x+w])
 
-        eyes_hist.append(extract_histogram(img[y:y+h, x:x+w]))
-        # cv2.rectangle(face,(x,y),(x+w,y+h),(0,255,0),2)
-
-        f.savefig("eye.png")
-
+        eyes_hist = np.concatenate((eye_1, eye_2))
+    else:
+        x, y, w, h = eyes[0]
+        eye_1 = extract_histogram(img[y:y+h, x:x+w])
+        eye_2 = np.zeros(len(eye_1))
+        eyes_hist = np.concatenate((eye_1, eye_2))
 
     # nose
     nose = nose_detector.detectMultiScale(img, scaleFactor=1.1, minNeighbors=3)
     if len(nose) != 0:
         x, y, w, h = nose[0]
         nose_hist = extract_histogram(img[y:y+h, x:x+w])
-        # cv2.rectangle(face,(x,y),(x+w,y+h),(0,255,0),2)
-        plt.imshow(img[y:y+h, x:x+w])
-
-        f.savefig("nose.png")
+    else:
+        nose_hist = np.zeros(len(nose))
 
     # mouth
     mouth = mouth_detector.detectMultiScale(img, scaleFactor=1.1, minNeighbors=3)
     if len(mouth) != 0:
         x, y, w, h = mouth[0]
         mouth_hist = extract_histogram(img[y:y+h, x:x+w])
-        # cv2.rectangle(face,(x,y),(x+w,y+h),(0,255,0),2)
-        plt.imshow(img[y:y+h, x:x+w])
+    else:
+        mouth_hist = np.zeros(len(mouth))
+    print(len(face_hist), len(eyes_hist), len(nose_hist), len(mouth_hist))
 
-        f.savefig("mouth.png")
+    # concatenate hists
+    conc_hist = np.concatenate([face_hist, eyes_hist, nose_hist, mouth_hist])
 
+    return conc_hist
 
 def extract_histogram(img):
     tmp_model = cv2.face.LBPHFaceRecognizer_create(
@@ -65,7 +67,7 @@ def extract_histogram(img):
                     grid_x = 8, # The number of cells in the horizontal direction, 8 is a common value used in publications. The more cells, the finer the grid, the higher the dimensionality of the resulting feature vector
                     grid_y = 8, # The number of cells in the vertical direction, 8 is a common value used in publications. The more cells, the finer the grid, the higher the dimensionality of the resulting feature vector
                 )  
-    #img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     tmp_model.train([img], np.array([0]))
     histogram = tmp_model.getHistograms()[0][0]
 
@@ -85,13 +87,18 @@ for i in range(0, len(X)):
         if y[i] == y[j]:    
             img_i = X[i]
             img_j = X[j]
-            capture_face_features(img_i)
-            capture_face_features(img_j)
 
-            # img_i = detect_face(X[i])
-            # img_j = detect_face(X[j])
+            img_i_hist = capture_face_features(img_i)
+            img_j_hist = capture_face_features(img_j)
 
-            print(get_correlation_between_two(extract_histogram(img_i), extract_histogram(img_j)))
+            # axis_values = np.array([i for i in range(0, len(img_i_hist))])
+            # fig = plt.figure()
+            # plt.bar(axis_values, img_i_hist)
+            # plt.show()    
+            
+            print(len(img_i_hist), len(img_j_hist))        
+
+            print(get_correlation_between_two(img_i_hist, img_j_hist))
             
             fig = plt.figure()
  
