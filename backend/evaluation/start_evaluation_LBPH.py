8226354462a1b957cba8afb1d69a502f77b9712a
Vasile Vladut Gorea <goreavladut@gmail.com>
2023-01-16 22:57:17 +0100

    added filters to LBPH

--- backend/evaluation/start_evaluation_LBPH.py
+++ backend/evaluation/start_evaluation_LBPH.py
@@ -1,13 +1,13 @@
 from .evaluation import compute_similarities, open_set_identification_eval, verification_eval, verification_mul_eval
-from sklearn.datasets import fetch_olivetti_faces
+from sklearn.datasets import fetch_lfw_people, fetch_olivetti_faces
 import numpy as np
-from .plots import roc_auc_curve, far_frr_curve
+from .plots import save_plots
 from tqdm import tqdm
 from scipy.spatial.distance import cosine
 import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
-
+import tensorflow as tf
 import matplotlib.pyplot as plt
 import cv2
 from scipy.stats import pearsonr
@@ -28,22 +28,11 @@ def extract_histogram(img):
 def get_correlation_between_two(hist1, hist2):
     return pearsonr(hist1, hist2)[0]
 
-from .evaluation import compute_similarities, open_set_identification_eval, verification_eval, verification_mul_eval
-from sklearn.datasets import fetch_lfw_people, fetch_olivetti_faces
-import numpy as np
-from .plots import save_plots
-from tqdm import tqdm
-from scipy.spatial.distance import cosine
-import os
-import pandas as pd
-from sklearn.model_selection import train_test_split
-import cv2
-
 DATASET = "LFW" #Dataset ot use: LFW or OLIVETTI
 
 ####### Loading and parsing the dataset images #######
 if DATASET == "LFW":
-    lfw_people = fetch_lfw_people(color=True, min_faces_per_person=10, resize=0.5)
+    lfw_people = fetch_lfw_people(color=True, min_faces_per_person=4, resize=0.5)
     X = lfw_people.images
     y = lfw_people.target
     X = np.array(X * 255, dtype='uint8')
@@ -59,14 +48,43 @@ else:
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=0)
 
 ######## Defining the paths where results will be saved ######## 
-SAVED_ARRAYS_PATH = "./evaluation/saved_arrays_lbph_lfw" if DATASET == "LFW" else "./evaluation/saved_arrays_lbph_olivetti"
+SAVED_ARRAYS_PATH = "backend/evaluation/saved_arrays_lbph_lfw" if DATASET == "LFW" else "./evaluation/saved_arrays_lbph_olivetti"
 PLOTS = os.path.join(SAVED_ARRAYS_PATH, "lfw_plots") if DATASET == "LFW" else os.path.join(SAVED_ARRAYS_PATH, "olivetti_plots")
 GALLERY_SET = os.path.join(SAVED_ARRAYS_PATH, "gallery_set.npy")
+GALLERY_LABEL = os.path.join(SAVED_ARRAYS_PATH, "gallery_label.npy")
 PROBE_SET = os.path.join(SAVED_ARRAYS_PATH, "probe_set.npy")
+PROBE_LABEL = os.path.join(SAVED_ARRAYS_PATH, "probe_label.npy")
 SIMILARITIES_PATH = os.path.join(SAVED_ARRAYS_PATH, "similarities.npy")
 IDENTIFICATION_METRICS = os.path.join(SAVED_ARRAYS_PATH, "identification_metrics.csv")
 VERIFICATION_METRICS = os.path.join(SAVED_ARRAYS_PATH, "verification_metrics.csv")
 VERIFICATION_MUL_METRICS = os.path.join(SAVED_ARRAYS_PATH, "verification_mul_metrics.csv")
+
+def apply_filters(filter, template):
+        """
+        Apply different filters to increase the face features
+        """
+        image = tf.cast(tf.convert_to_tensor(template), tf.uint8)
+        
+        # Boosting constrast
+        if filter == 0:
+            contrast = tf.image.adjust_contrast(image, 0.8)
+            return np.array(contrast* 255, dtype='uint8') 
+        elif filter == 1:
+            contrast = tf.image.adjust_contrast(image, 0.9)
+            return np.array(contrast* 255, dtype='uint8') 
+        elif filter == 2:
+            contrast = tf.image.adjust_contrast(image, 1)
+            return np.array(contrast* 255, dtype='uint8')
+        # Boosting brightness        
+        elif filter == 3:
+            brightness = tf.image.adjust_brightness(image, 0.1)
+            return np.array(brightness* 255, dtype='uint8')
+        elif filter == 4:
+            brightness = tf.image.adjust_brightness(image, 0.2)
+            return np.array(brightness* 255, dtype='uint8')
+        elif filter == 5:
+            brightness = tf.image.adjust_brightness(image, 0.3)
+            return np.array(brightness* 255, dtype='uint8')  
 
 if not os.path.exists(SAVED_ARRAYS_PATH):
     os.mkdir(SAVED_ARRAYS_PATH)
@@ -76,24 +94,42 @@ if not os.path.exists(PLOTS):
 ######## Build feature vectors ########
 gallery_set = []
 probe_set = []
+galery_label = []
+probe_label = []
 
 if os.path.exists(GALLERY_SET):
     gallery_set = np.load(GALLERY_SET)
+    galery_label = np.load(GALLERY_LABEL)
 else:
-    for gallery_template in tqdm(X_train, desc="Extracting gallery set feature vectors"):
+    for index,gallery_template in enumerate(tqdm(X_train, desc="Extracting gallery set feature vectors")):
         gallery_set.append(extract_histogram(gallery_template))
+        
+        #apply filters
+        for i in range(6):
+            template=apply_filters(i,gallery_template)
+            gallery_set.append(extract_histogram(template))
+            galery_label.append(y_train[index])
     np.save(GALLERY_SET, np.array(gallery_set))
+    np.save(GALLERY_LABEL, np.array(galery_label))
 
 if os.path.exists(PROBE_SET):
     probe_set = np.load(PROBE_SET)
+    probe_label = np.load(PROBE_LABEL)
 else:
-    for probe_template in tqdm(X_test, desc="Extracting probe set feature vectors"):
+    for index,probe_template in enumerate(tqdm(X_test, desc="Extracting probe set feature vectors")):
         probe_set.append(extract_histogram(probe_template))
+        
+        #apply filters
+        for i in range(6):
+            template=apply_filters(i,probe_template)
+            probe_set.append(extract_histogram(template))
+            probe_label.append(y_test[index])
     np.save(PROBE_SET, np.array(probe_set))
+    np.save(PROBE_LABEL, np.array(probe_label))
 
 # Each element is of type (label, feature_vector)
-gallery_data = np.array([(y_train[i], gallery_set[i]) for i in range(len(gallery_set))])
-probe_data = np.array([(y_test[i], probe_set[i]) for i in range(len(probe_set))])
+gallery_data = np.array([(galery_label[i], gallery_set[i]) for i in range(len(gallery_set))])
+probe_data = np.array([(probe_label[i], probe_set[i]) for i in range(len(probe_set))])
 
 ######## Load similarity matrix if present on disk ######## 
 if os.path.exists(SIMILARITIES_PATH):
